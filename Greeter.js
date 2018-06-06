fe9a6ebe55c9cca022592924eb4236ef9632e437
Charles Hsu <charles@6e.com.tw>
2018-06-07 07:42:55 +0800

    first commit

---
+++ Greeter.js
@@ -0,0 +1,6 @@
+(function (global, $) {
+  console.log("Greeter.js is running!");
+  console.log("global", global);
+  console.log("$", $);
+}(window, jQuery));
+
---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("App.js is running!");
+var q = $("ul.people").addClass("newClass").removeClass("people");
